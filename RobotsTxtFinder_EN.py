7fd59e05c7ad4a6c0ca8471815d765f2c0506799
L'HommeDeLaSituation <126177599+lhommedelasituation@users.noreply.github.com>
2023-03-08 12:18:48 +0100

    Create RobotsTxtFinder_EN.py

---
+++ RobotsTxtFinder_EN.py
@@ -0,0 +1,67 @@
+import asyncio
+import requests
+import argparse
+
+
+async def check_robot_async(url, user_agent, timeout):
+    try:
+        host = requests.utils.urlparse(url).hostname
+        robots_url = f'http://{host}/robots.txt'
+        async with aiohttp.ClientSession() as session:
+            async with session.get(robots_url, headers={'User-Agent': user_agent}, timeout=timeout) as response:
+                if response.status == 200:
+                    return url
+    except:
+        pass
+
+
+def check_robot_sync(url, user_agent, timeout):
+    try:
+        host = requests.utils.urlparse(url).hostname
+        robots_url = f'http://{host}/robots.txt'
+        response = requests.get(robots_url, headers={'User-Agent': user_agent}, timeout=timeout)
+        if response.status_code == 200:
+            return url
+    except:
+        pass
+
+
+async def main(urls, user_agent, concurrency, timeout, sync):
+    accessible_robot_urls = []
+    tasks = []
+
+    if sync:
+        check_robot = check_robot_sync
+    else:
+        check_robot = check_robot_async
+
+    async with asyncio.Semaphore(concurrency):
+        for url in urls:
+            task = asyncio.ensure_future(check_robot(url, user_agent, timeout))
+            tasks.append(task)
+
+        for completed_task in asyncio.as_completed(tasks):
+            result = await completed_task
+            if result is not None:
+                accessible_robot_urls.append(result)
+
+    with open('accessible_robot_urls.txt', 'w') as f:
+        for url in accessible_robot_urls:
+            f.write(f'{url}\n')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Check if robot.txt is accessible for a given set of URLs')
+    parser.add_argument('urls_file', help='path to file containing URLs to check (e.g. /path/to/urls.txt)')
+    parser.add_argument('--user-agent', '-ua', default='Python requests', help='user agent to use (default: Python requests)')
+    parser.add_argument('--concurrency', '-c', type=int, default=10, help='maximum number of concurrent requests (default: 10)')
+    parser.add_argument('--timeout', '-t', type=int, default=5, help='maximum timeout for each request in seconds (default: 5)')
+    parser.add_argument('--sync', action='store_true', help='use synchronous requests instead of async (default: False)')
+    args = parser.parse_args()
+
+    user_agent = args.user_agent
+    url_list = [line.rstrip('\n') for line in open(args.urls_file)]
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main(url_list, user_agent, args.concurrency, args.timeout, args.sync))
+    loop.close()
